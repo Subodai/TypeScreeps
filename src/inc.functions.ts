72ac08587d86c6bc790a77c37690e80875bad325
Mark Thompson <mthompson@frontier.co.uk>
2018-03-20 15:09:17 +0000

    First commit

---
+++ src/inc.functions.ts
@@ -0,0 +1,57 @@
+export class Functions {
+
+    private static c_grey:string   = '#CCCCCC';
+    private static c_red:string    = '#FF0000';
+    private static c_green:string  = '#00FF00';
+    private static c_blue:string   = '#0000FF';
+    private static c_yellow:string = '#FFFF00';
+
+    /**
+     * Debug messages for creeps, will spit out room details and creep details
+     *
+     * @param message {string}
+     * @param creep {Creep}
+     */
+    public static CreepMsg(message: string, creep: Creep) {
+        let msg: string = '';
+        msg += '<span style="color:' + this.c_grey + ';">[' + Game.time + ']</span> ';
+        let room: Room = creep.room;
+        if (room) {
+            msg += '<span style="color:' + this.c_yellow + ';">[' + room.name + ']</span> ';
+        }
+        if (creep) {
+            msg += '<span style="color:' + this.c_green + ';">[' + creep.name + ']</span> ';
+        }
+        msg += message;
+        console.log(msg);
+    }
+
+    /**
+     * Debug messages for rooms, will spit out room details
+     *
+     * @param message {string}
+     * @param room {Room}
+     */
+    public static RoomMsg(message: string, room: Room) {
+        let msg: string = '';
+        msg += '<span style="color:' + this.c_grey + ';">[' + Game.time + ']</span> ';
+        if (room) {
+            msg += '<a href="/a/#!/room/shard1/' + room.name + '"><span style="color:' + this.c_yellow + ';">[' + room.name + ']</span></a> ';
+        }
+        msg += message;
+        console.log(msg);
+    }
+
+    /**
+     * Debug Messages with niceness
+     *
+     * @param message {string}
+     */
+    public static DebugMsg(message: string) {
+        let msg: string = '';
+        msg += '<span style="color:' + this.c_grey + ';">[' + Game.time + ']</span> ';
+        msg += message;
+        console.log(msg);
+    }
+}
+
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { ErrorMapper } from "utils/ErrorMapper";
-
+import { Functions } from "inc.functions";
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
-  console.log(`Current game tick is ${Game.time}`);
+    Functions.DebugMsg(`Current game tick is ${Game.time}`);
 
-  // Automatically delete memory of missing creeps
-  for (const name in Memory.creeps) {
-    if (!(name in Game.creeps)) {
-      delete Memory.creeps[name];
+    for (const room in Game.rooms) {
+        Functions.RoomMsg('Is my room', Game.rooms[room]);
     }
-  }
+    // Automatically delete memory of missing creeps
+    for (const name in Memory.creeps) {
+        if (!(name in Game.creeps)) {
+            delete Memory.creeps[name];
+        }
+    }
 });
