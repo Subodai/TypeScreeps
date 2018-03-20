4920d298f7d21b22e3987b4e9dfb1ec0161b0605
Mark Thompson <subodai85@gmail.com>
2018-03-20 21:37:36 +0000

    test commit

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
-import { ErrorMapper } from "utils/ErrorMapper";
-import { Debug } from "functions/debug";
+/* load functions */
+import { Debug } from "./functions/debug";
+
+/* load prototypes */
+import { loadCreepPrototypes } from "./prototypes/creep";
+import { loadRoomPrototypes } from "./prototypes/room";
+
+/* load error mapper */
+import { ErrorMapper } from "./utils/ErrorMapper";
+
+loadRoomPrototypes();
+loadCreepPrototypes();
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
     Debug.DebugMsg(`Current game tick is ${Game.time}`);
 
     for (const room in Game.rooms) {
-        Debug.RoomMsg('Is my room', Game.rooms[room]);
+        Debug.RoomMsg("Is my room", Game.rooms[room]);
     }
     // Automatically delete memory of missing creeps
     for (const name in Memory.creeps) {
