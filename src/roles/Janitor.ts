8e4e0e37bc143922ae5b5ec6d5b4c6727d77e71f
Mark Thompson <mthompson@frontier.co.uk>
2018-05-21 14:07:31 +0100

    part 1

--- src/roles/Janitor.ts
+++ src/roles/Janitor.ts
@@ -1,3 +1,48 @@
+import * as STATE from "config/states";
+import { BodyBuilder } from "functions/tools";
+
 /**
  * Wall and Rampart Builder
  */
+export class Janitor {
+    public static ticksBeforeRenew: number = 150;
+    public static colour: string = "#006699";
+    public static roleName: string = "janitor";
+    public static roster: number[] = [ 0, 0, 0, 0, 0, 0, 0, 1, 1 ];
+    public static bodyStructure: BodyPartConstant[][] = [
+        [], [], [], [], [], [], [],
+        BodyBuilder({ WORK: 4, CARRY: 2, MOVE: 6 }),
+        BodyBuilder({ WORK: 4, CARRY: 2, MOVE: 6 })
+    ];
+
+    public static enabled(room: Room): boolean {
+        if (room.controller) {
+            if (room.controller.level >= 7 && room.storage) {
+                // @todo move to own method
+                // get all walls and ramparts below their max hp
+                const items: Structure[] = room.find(FIND_STRUCTURES, {
+                    filter: (s: AnyStructure) =>
+                        // ramparts below max
+                        (s.structureType === STRUCTURE_RAMPART && s.hits < global.rampartMax) ||
+                        // walls below max
+                        (s.structureType === STRUCTURE_WALL && s.hits < global.wallMax) ||
+                        // anything else
+                        (
+                            // not a wall, rampart or road
+                            (
+                                s.structureType !== STRUCTURE_WALL &&
+                                s.structureType !== STRUCTURE_RAMPART &&
+                                s.structureType !== STRUCTURE_ROAD
+                            ) &&
+                            // with less than 100hp
+                            s.hits < s.hitsMax
+                        )
+                });
+                if (items.length > 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
--- src/types/role.d.ts
+++ src/types/role.d.ts
@@ -52,6 +52,7 @@ declare class RemoteReserver extends Role {}
 declare class RemoteEnergyMiner extends Role {}
 declare class RemoteClaimer extends Role {}
 declare class MineralExtractor extends Role {}
+declare class Janitor extends Role {}
 
 /**
  * Defines what a creep does during it's lifespan
@@ -67,7 +68,8 @@ type CreepRole = Role
     | RemoteReserver
     | RemoteEnergyMiner
     | RemoteClaimer
-    | MineralExtractor;
+    | MineralExtractor
+    | Janitor;
 /**
  * !!!! NOTICE !!!! Remember to update ROLES, Counter, Runner and Room.ts
  */
