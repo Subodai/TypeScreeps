c4398e25140e44846a295ec1a1d40c1f6452e584
Mark Thompson <subodai85@gmail.com>
2018-09-08 23:14:02 +0100

    need 2 refiller during link time.

--- src/roles/Refiller.ts
+++ src/roles/Refiller.ts
@@ -6,7 +6,7 @@ export class Refiller {
     public static colour: string = "#888888";
     public static roleName: string = "refil";
     public static roster: number[]      = [ 0, 0, 0, 0, 1, 1, 1, 1, 1 ];
-    public static rosterLinks: number[] = [ 0, 0, 0, 0, 1, 1, 1, 1, 1 ];
+    public static rosterLinks: number[] = [ 0, 0, 0, 0, 2, 2, 2, 2, 1 ];
     public static bodyStructure: BodyPartConstant[][] = [
         [],
         [],
